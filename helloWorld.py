148a5b5b04a733bb84685544a696d50fa253d89b
Rohit <Rohit Aute>
2026-04-01 21:55:05 -0700

    Describe what you changed

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,11 @@
-print ("Hello World")
-print ("Welcome to Python programming!")
+import random
+print("Hello, welcome to Rock, Papper, Scissors Game!")
+first = input("Enter your choice (rock, paper, scissors): ")
+options = ["rock", "paper", "scissors"]
+second = random.choice(options)
+print(random.choice(options))
+if first == second:
+    print("It's a tie!")
+elif (first == "rock" and second == "scissors") or (first == "paper" and second == "rock") or (first == "scissors" and second == "paper"):
+    print("You win!")   
+else:    print("You lose!")
