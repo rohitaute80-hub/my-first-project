4e6c6653d163aca7fc1a0d9ba901b5f47267fc5e
Rohit <Rohit Aute>
2026-04-02 15:42:23 -0700

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,34 @@
-print ("Hello World")
-print ("Welcome to Python programming!")
+import random
+
+print("Let's play Rock, Paper, Scissors")
+first = input("Player 1, please enter your choice: Rock, Paper or Scissors: ")
+options = ["Rock", "Paper", "Scissors"]
+
+if first not in options:
+    print("Invalid choice, please choose Rock, Paper or Scissors")
+else:
+    second = random.choice(options)
+    print("Player 2 chooses:", second)
+
+    if first == "Rock":
+        if second == "Rock":
+            print("It's a tie!")
+        elif second == "Paper":
+            print("Player 2 wins!")
+        else:
+            print("Player 1 wins!")
+    elif first == "Paper":
+        if second == "Rock":
+            print("Player 1 wins!")
+        elif second == "Paper":
+            print("It's a tie!")
+        else:
+            print("Player 2 wins!")
+    elif first == "Scissors":
+        if second == "Rock":
+            print("Player 2 wins!")
+        elif second == "Paper":
+            print("Player 1 wins!")
+        else:
+            print("It's a tie!")
+
